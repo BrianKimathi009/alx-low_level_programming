d8751959f034de3187256e666029d1df2b0ea064
BrianKimathi009 <lithimbibrian9.com>
2023-05-18 19:43:22 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly and that piece of
+ * art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: 1
+ */
+int main(void)
+{
+
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
